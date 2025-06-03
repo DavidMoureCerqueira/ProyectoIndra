acf3b137aac1f59f1ca8e791be0b8fbbc9adfaea
David Moure Cerqueira <mourefp@gmail.com>
2025-06-03 12:43:58 +0200

    Carrusel
    Subo el js del Carrusel

---
+++ js/carrusel.js
@@ -0,0 +1,16 @@
+let indice = 0;
+const carrusel = document.getElementById('carrusel');
+const total = carrusel.children.length;
+const contenedor = document.querySelector('.carrusel-container');
+
+function moverCarrusel(direccion) {
+  const ancho = contenedor.clientWidth;
+  indice = (indice + direccion + total) % total;
+  carrusel.style.transform = `translateX(-${indice * ancho}px)`;
+}
+
+// Avance automático cada 4 segundos
+setInterval(() => {
+  moverCarrusel(1);
+}, 4000);
+
